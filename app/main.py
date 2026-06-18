c3cde890772c7d618456be566127dca61e5c4b32
KNDiwyanjali <kndiwya@gmail.com>
2026-06-18 13:51:29 +0530

    fastapi is developped and running

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="FastAPI Template",
+    description="A simple FastAPI  template " ,
+    version="1.0.0",
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to FastAPI Template!"}
+
+@app.get("/health")
+def read_health():
+    return {"status": "ok"}
