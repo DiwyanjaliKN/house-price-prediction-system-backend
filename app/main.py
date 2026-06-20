efe27196a001693d9ac188a2baa58f6f94e659b4
KNDiwyanjali <kndiwya@gmail.com>
2026-06-20 15:30:49 +0530

    "model loaded to the program successfully"

--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI
 
+
+
 app = FastAPI(
     title="FastAPI Template",
-    description="A simple FastAPI  template " ,
+    description="A simple FastAPI template.",
     version="1.0.0",
 )
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to FastAPI Template!"}
+    return {"message": "welcome to FastAPI Template!"}
 
 @app.get("/health")
 def read_health():
     return {"status": "ok"}
+
