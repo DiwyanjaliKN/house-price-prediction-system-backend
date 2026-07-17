ccdc98b45239746f0dd1249436c64d7613540168
K.N.Diwyanjali <kndiwya@gmail.com>
2026-07-17 05:56:20 +0530

    fixxing

--- backend/app/main.py
+++ backend/app/main.py
@@ -35,7 +35,7 @@ app = FastAPI(
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[frontend_url],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
